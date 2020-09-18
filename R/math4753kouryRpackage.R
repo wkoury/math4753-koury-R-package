a14eeeffc57709ec0be7adedd85a68c7c4115f9d
Will Koury <wgkoury@gmail.com>
2020-09-18 11:30:18 -0500

    Attempts to create an R function

---
+++ R/math4753kouryRpackage.R
@@ -0,0 +1,10 @@
+#' Koury Package
+#'
+#' We are going to put a lot of stuff here in this class.
+#'
+#' @docType package
+#'
+#' @author Will Koury \email{wkoury@ou.edu}
+#'
+#' @name math4753kouryRPackage
+NULL
