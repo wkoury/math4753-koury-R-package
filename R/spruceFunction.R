9a6cbbbf7ecd1db2a807084e8841398ea7ee91ff
Will Koury <wgkoury@gmail.com>
2020-09-18 14:00:12 -0500

    Removes returnable

--- R/spruceFunction.R
+++ R/spruceFunction.R
@@ -13,5 +13,4 @@ plotSpruce <- function(filename) {
   plot(Height~BHDiameter,bg="Blue",pch=21,cex=1.2,
        ylim=c(0,max(Height)),xlim=c(0,max(BHDiameter)),
        main="Spruce height prediction",data=spruce.df)
-  return(NULL)
 }
